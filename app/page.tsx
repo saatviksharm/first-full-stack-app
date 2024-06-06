392bd814623c4a450c2d75fd9cbf29ce6407c402
saatviksharm <saatvik.sharma@gmail.com>
2024-06-06 18:01:08 -0400

    Used react hooks and created base api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+'use client'
+
+import { log } from "console";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  function asyncFunctionTest() {
+    fetch('/api/random-number', {
+      method: 'GET'
+    }).then(a => a.json()).then(b => {
+      console.log(b);
+    })
+  }
+
+  const [test, setTest] = useState<String>("on");
+  
+  useEffect(() => {
+    console.log('hello');
+  }, [])
+
   return (
-    <div>Hello World</div>
+    <div>
+      Hello World
+      <button onClick={() => asyncFunctionTest()}>
+        {test}
+      </button>
+    </div>
+    
   )
 }
+
