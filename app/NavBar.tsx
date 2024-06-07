729005f362f34fed61e03c2c6058e72997acbb28
Saatvikbot123 <saatvik.sharma@gmail.com>
2024-06-07 16:49:51 -0400

    push put

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,39 +1,41 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { FaBug } from "react-icons/fa";
-import classnames from 'classnames';
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
 
-    const currentPath = usePathname();
-    
-
-    const links =[
-        {label: 'Dashboard', href: '/'},
-        {label: 'Issues', href: '/issues'},
-    ]
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 h-10 px-5 items-center'>
-        <Link href="/">
-            <FaBug></FaBug>
-        </Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-            <Link 
-                key={link.href} 
-                className={classnames({
-                    'text-zinc-900': link.href === currentPath,
-                    'text-zinc-500': link.href !== currentPath,
-                    'hover:text-zinc-800 transition-colors': true
-                })} 
-                href={link.href}>{link.label}</Link>)}
-            
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 h-10 px-5 items-center">
+      <Link href="/">
+        <FaBug></FaBug>
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900 border border-zinc-900": link.href === currentPath,
+              "text-zinc-500 border border-zinc-500": link.href !== currentPath,
+              "hover:bg-zinc-200 transition-colors focus-within:bg-xinc-200 outline-none px-2 py-1":
+                true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
